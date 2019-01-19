a21a724dd3c9221bcaa8b2ff2398b5fd56eee60c
Ankit-singh72 <Ankit-singh7>
2019-01-19 18:06:42 +0530

    third commit

--- index.js
+++ index.js
@@ -81,7 +81,8 @@ fs.readdirSync(modelsPath).forEach(function(file)
 
 
  //creating HTTP server
- const server=http.createServer(app);
+ const server=app.listen(3000);
+ const io=require('socket.io').listen(server);
 
  //starting listening to http server required by socket
  console.log(appConfig);
