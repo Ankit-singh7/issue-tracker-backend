2f2da3956ca75f247b8fcad8feebd2865f8efb61
Ankit-singh72 <Ankit-singh7>
2019-01-26 02:11:15 +0530

    first commit

--- index.js
+++ index.js
@@ -37,6 +37,16 @@ const controllersPath = './controllers';
 const libsPath = './libs';
 const middlewaresPath = './middlewares';
 const routesPath = './routes';
+
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  next();
+});
+
+
 // Bootstrap route
 fs.readdirSync(routesPath).forEach(function (file) {
   if (~file.indexOf('.js')) {
