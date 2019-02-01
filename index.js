f133d0290e0bcebd250927796242d448adf6b7e3
Ankit-singh72 <Ankit-singh7>
2019-02-01 15:27:40 +0530

    commit

--- index.js
+++ index.js
@@ -22,12 +22,6 @@ const app = express()
 app.use(morgan('dev'));
 
 
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    next();
-});
 
 
 //middlewares
